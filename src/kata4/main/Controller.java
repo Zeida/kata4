268672e14e4c3c68dbc13ca52083036c1e01b0f8
45395507 <45395507@DESKTOP-3D5M5VE>
2019-01-16 23:59:41 +0000

    kata4 Version2

--- src/kata4/main/Controller.java
+++ src/kata4/main/Controller.java
@@ -1,19 +1,35 @@
 package kata4.main;
-import java.util.*;
-import kata4.model.*;
+
+import java.io.IOException;
+import java.util.List;
+import kata4.model.Histogram;
 import kata4.view.HistogramDisplay;
 import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
-import java.io.IOException;
 
 public class Controller {
-    public static void main(String[] args) throws IOException{        
-        String fileName = "src/File/emailsfile.txt";
+    private static List<String> mailList;
+    private static Histogram<String> histogram;
 
-        List<String> mailList = MailListReader.read(fileName);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+    public static void main(String[] args) {
+        execute();
+    }
 
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-        histoDisplay.execute();
+    private static void execute(){
+        Input();
+        Process();
+        Output();
+    }
+
+    private static void Input(){
+        mailList = MailListReader.read("src/File/emailsfile.txt");
+    }
+
+    private static void Process(){
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+
+    private static void Output(){
+        (new HistogramDisplay(histogram)).execute();
     }
 }
